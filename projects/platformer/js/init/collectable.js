68216d1cfc8db441a74f31d3b3e5270c535c4c70
logenab <ayronb810@gmail.com>
2022-09-29 00:58:51 +0000

    <message about commit>

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -14,35 +14,18 @@
 
   window.opspark.collectable.type = type;
 
-  /**
-   * init: Initialize all collectables.
-   *
-   * GOAL: Add as many collectables as necessary to make your level challenging.
-   *
-   * Use the createCollectable() Function to create collectables for the level.
-   * See the type Object, above, for the types of collectables and their point values.
-   *
-   * createCollectable() takes these arguments:
-   *
-   *      createCollectable(type, x, y, gravity, bounce);
-   *
-   *      type: The type of the collectable, use the type Object above.
-   *      x: The x coordineate for the collectable.
-   *      y: The y coordineate for the collectable.
-   *      gravity: OPTIONAL The gravitational pull on the collectable.
-   *      bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
-   */
+  
   function init(game) {
     let createCollectable = collectable.create;
 
-    ////////////////////////////////////////////////////////////////////////
-    // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-
-    // example:
+   
     createCollectable(type.steve, 200, 170, 6, 0.7);
 
-    // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
+    createCollectable(type.steve, 500, 250, 8, 0.2);
+
+    createCollectable(type.steve, 400, 100, 1, 0.7);
+    
+    createCollectable(type.steve, 300, 120, 9, 10);
   }
   collectable.init = init;
 })(window);
--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -5,24 +5,7 @@
 
   let platform = window.opspark.platform;
 
-  /**
-   * init: This function initializes the platforms for the level.
-   *
-   * GOAL: Add as many platforms necessary to make your level challenging.
-   *
-   * Use the createPlatform Function to create platforms for the level.
-   *
-   * createPlatform() takes these arguments:
-   *
-   *      createPlatform(x, y, scaleX, scaleY);
-   *
-   *      x: The x coordineate for the platform.
-   *      y: The y coordineate for the platform.
-   *      scaleX: OPTIONAL The scale factor on the x-axis, this value will
-   *              stretch the platform in width.
-   *      scaleY: OPTIONAL The scale factor on the y-axis, this value will
-   *              stretch the platform in height.
-   */
+  
   function init(game) {
     let createPlatform = platform.create;
 
@@ -37,8 +20,10 @@
     createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
 
     // example:
-    createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
+    createPlatform(400, 500);
+    createPlatform(125, 400, 0.6);
+    createPlatform(120, 100, 0.6);
+    createPlatform(140, 590, 0.7);
 
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
