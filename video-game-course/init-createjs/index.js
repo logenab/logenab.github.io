7b0711c886471bd9951333ec11181d2728801985
logenab <ayronb810@gmail.com>
2024-02-28 01:55:28 +0000

    shapes

--- video-game-course/init-createjs/index.js
+++ video-game-course/init-createjs/index.js
@@ -5,7 +5,7 @@
 (function (window, createjs) {
   // TODO 5: Initialize CreateJS //
   console.log("index.js intialized");
-  const canvas = document.getElementById('canvas');
+  const canvas = document.getElementById("canvas");
   const stage = new createjs.Stage(canvas);
   // TODO 6: Set the framerate of the Ticker
   createjs.Ticker.framerate = 60;
@@ -14,7 +14,9 @@
    * TODO 7:CREATE AND CONFIGURE ANY DISPLAY
    * OBJECTS AND ADD THEM TO THE DISPLAY LIST HERE
    */
-
+  shapeOne = new createjs.Shape();
+  shapeTwo = new createjs.Shape();
+  shapeThree = new createjs.Shape();
   // INIT CREATEJS //
   const radius = 25;
   const margin = 125;
@@ -22,16 +24,18 @@
   // CREATE A BACKGROUND //
   const background = new createjs.Shape();
   background.graphics
-    .beginFill("powderblue")
+    .beginFill("black")
     .drawRect(0, 0, canvas.width, canvas.height);
 
   // CREATE A CIRCLE //
-
+  shapeOne.graphics.beginFill("purple").drawCircle(250, 100, radius);
+  shapeTwo.graphics.beginFill("purple").drawCircle(120, 100, radius);
+  shapeThree.graphics.beginFill("purple").drawRect(250, 200, radius);
   // ADD DISPLAY OBJECTS TO STAGE //
-stage.addChild();
+  stage.addChild(background, shapeOne,shapeTwo,shapeThree);
+
   // TODO 8: Listen to the 'tick' event  //
-  let tickerHandler = createjs.Ticker.on('tick', onTick);
-  
+  let tickerHandler = createjs.Ticker.on("tick", onTick);
 
   // TODO 9: Handle the 'tick' event //
   function onTick(event) {
